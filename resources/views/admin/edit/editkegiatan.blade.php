a71ee38830246f15f38d3b67203dcbbdc9145898
roqib3 <117055751+roqib3@users.noreply.github.com>
2024-08-30 05:03:59 +0700

    styling edit berita, kegiatan, foto

--- resources/views/admin/edit/editkegiatan.blade.php
+++ resources/views/admin/edit/editkegiatan.blade.php
@@ -1,19 +1,26 @@
 @extends('/admin/dashboard')
+@section('title', 'Kelola Informasi / Kegiatan / Edit')
 @section('content')
-    <form action="/kegiatan/update/{{ $kegiatan->id }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="title">Judul Kegiatan</label>
-            <input type="text" class="form-control" id="judul_kegiatan" name="judul_kegiatan" value="{{ $kegiatan->judul_kegiatan }}" required>
+<div class="container" style="min-height: 80vh">
+    <div class="card rounded-3 shadow-sm">
+        <div class="card-body mx-4 my-2">
+            <form action="/kegiatan/update/{{ $kegiatan->id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Judul Kegiatan</label>
+                    <input type="text" class="form-control" id="judul_kegiatan" name="judul_kegiatan" value="{{ $kegiatan->judul_kegiatan }}" required>
+                </div>
+                <div class="form-group">
+                    <label for="image">Gambar Kegiatan</label>
+                    <input type="file" class="form-control-file" id="gambar_kegiatan" name="gambar_kegiatan" required>
+                </div>
+                <div class="form-group">
+                    <label for="description">Deskripsi Kegiatan</label>
+                    <textarea class="formroens-control" id="deskripsi_kegiatan" name="deskripsi_kegiatan" rows="5" required>{{ $kegiatan->deskripsi_kegiatan }}</textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="image">Gambar Kegiatan</label>
-            <input type="file" class="form-control-file" id="gambar_kegiatan" name="gambar_kegiatan" required>
-        </div>
-        <div class="form-group">
-            <label for="description">Deskripsi Kegiatan</label>
-            <textarea class="formroens-control" id="deskripsi_kegiatan" name="deskripsi_kegiatan" rows="5" required>{{ $kegiatan->deskripsi_kegiatan }}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+</div>
 @endsection
